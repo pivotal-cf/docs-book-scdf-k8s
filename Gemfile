a8fb0c625e53417cbf485f3071612f75ffb1f8f2
Miranda Limonczenko <mlimonczenko@pivotal.io>
2020-05-19 14:34:22 -0700

    First commit [#172797688]

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+gem 'therubyracer'
+gem 'bookbindery', "10.1.15"
+gem 'rake'
+gem 'jasmine'
---
+++ redirects.rb
@@ -0,0 +1,6 @@
+# Redirect all production http traffic to https
+r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
+  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+}
+
+r301 %r{/scdf/(?![\d-]+)(.*)}, "/scdf/1-5/$1"
