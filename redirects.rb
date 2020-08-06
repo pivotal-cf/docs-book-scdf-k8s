f19ca0c26ebd68dc9b64312ce03f0225f0ff0571
trisberg <trisberg@pivotal.io>
2020-08-06 12:36:22 -0400

    Make v1-1 default redirect

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/scdf-k8s/(?![\d-]+)(.*)}, "/scdf-k8s/1-0/$1"
+r301 %r{/scdf-k8s/(?![\d-]+)(.*)}, "/scdf-k8s/1-1/$1"
