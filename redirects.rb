a25600284c7b508465d3596a41c6bf0ab629d132
trisberg <trisberg@pivotal.io>
2020-12-03 14:12:02 -0500

    Change product name

--- redirects.rb
+++ redirects.rb
@@ -3,7 +3,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/scdf-k8s/(?![\d-]+)(.*)}, "/scdf-k8s/1-1/$1"
+r301 %r{/scdf-k8s/(?![\d-]+)(.*)}, "/scdf-k8s/1-2/$1"
 
 # Add subdirectory for Pro Features
 r301 '/scdf-k8s/1-2/multi-io-support.html', '/scdf-k8s/1-2/pro-features/multi-io-support.html'
