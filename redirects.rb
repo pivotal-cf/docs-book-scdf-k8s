d344095847386d6b5c500477b3f4a52f2d8f721a
Ben Klein <bklein@pivotal.io>
2021-11-22 18:05:09 -0600

    Redirect to VMware documentation site

--- redirects.rb
+++ redirects.rb
@@ -1,10 +1,2 @@
-# Redirect all production http traffic to https
-r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
-  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
-}
-
-r301 %r{/scdf-k8s/(?![\d-]+)(.*)}, "/scdf-k8s/1-4/$1"
-
-# Add subdirectory for Pro Features
-r301 '/scdf-k8s/1-2/multi-io-support.html', '/scdf-k8s/1-2/pro-features/multi-io-support.html'
-r301 '/scdf-k8s/1-2/container-registry-updates.html', '/scdf-k8s/1-2/pro-features/container-registry-updates.html'
+# Redirect all traffic to VMware site
+r301 %r{(.*)}, 'https://docs.vmware.com/en/VMware-Spring-Cloud-Data-Flow-for-Kubernetes/index.html'
